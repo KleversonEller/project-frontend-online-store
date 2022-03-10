8d39fb3844c043716fcc8174ff5d3decc1ae865e
João Vitor Souza Rocha <jvitorsouza18@gmail.com>
2022-03-10 16:21:54 -0300

    requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,23 @@
 export async function getCategories() {
-  // Implemente aqui
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const data = await response.json();
+  console.log(data);
+  return data;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+getCategories();
+
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  let data = '';
+  if (categoryId) {
+    const idResponse = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+    data = await idResponse.json();
+  } else if (query) {
+    const queryResponse = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
+    data = await queryResponse.json();
+  } else {
+    data = {};
+  }
+  return data;
 }
